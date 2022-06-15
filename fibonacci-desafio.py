c0f74841fefb74e74f9560bf36d4dfd0be597802
RafaelM42 <94637835+RafaelM42@users.noreply.github.com>
2022-06-15 18:25:23 -0300

    Update fibonacci-desafio.py

--- fibonacci-desafio.py
+++ fibonacci-desafio.py
@@ -1,16 +1,28 @@
 ## Rafael Mello, Marcelo Silveira e Luiz Gabriel Lima.
+## Plotar a sequência de fibonacci, na qual cada "sessão" tem uma cor.
+
+##Importar a biblioteca de plotagem gráfica.
+import matplotlib
+from matplotlib import pyplot as plt
+
+## Pegar o número de termos a ser encontrado
 n=int(input('Termo que deseja encontrar:'))
+## Os primeiros termos da sequência
 ultimo=1
 penultimo=1
 
+##Caso dos dois primeiros termos
 if(n==1) or (n==2):
     print("1")
 else:
+    ##Caso para demais números
     count=3
     #printar os "1" da sequência.
     print('1')
     print('1')
 
+## Laço de repetição para printar demais números até 
+## o que foi determinado pelo usuário.
 while (count <= n):
     termo=ultimo + penultimo
     penultimo=ultimo
